e2483d6bf49bbe128045535a9270e79777350236
bbhavii <bhavikapadia2000@gmail.com>
2022-11-11 17:21:48 +0530

    heap & sort (min heap)

---
+++ heap_sort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int * a, int * b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void heapify(int a[], int n, int h){
+    int largest = h;
+    int left = h*2 + 1;
+    int right = h*2 + 2;
+
+    if(left<n && a[left]>a[largest]){
+        largest=left;
+
+    }
+    if(right<n && a[right]>a[largest]){
+        largest=right;
+    }
+    if(largest != h){
+        swap(&a[right],&a[largest]);
+
+        heapify(a,n,largest);
+    }
+}
+
+void buildHeap(int a[], int n){
+
+    int head = (n/2)-1;
+    for(int i=head;i>=0;i--){
+        heapify(a,n,i);
+    }
+}
+
+void printHeap(int a[], int n){
+    printf("The heap is: \n");
+
+    for( int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+}
+
+int main(){
+    int a[] = {1,3,2,7,5};
+    buildHeap(a,5);
+    printHeap(a,5);
+    return 0;
+}
